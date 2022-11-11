bcc0fc4612a15b8e14dd857e5e0214e8fef985de
dunkeln <prateekpravanjan@gmail.com>
2022-11-11 22:16:45 +0300

    ...scaffolding

---
+++ y_2015/README.md
@@ -0,0 +1,10 @@
+<img src="../.assets/christmas_ferris.png" width="150px"/>
+
+# Advent of Code
+
+the repository is a workspace containing solutions to AoC challenges in rust. 
+It aims at aiding anyone stuck with a problem or curious about alternative solutions.
+
+currently a work in progress
+
+PRs are encouraged
--- y_2015/src/bin/05.rs
+++ y_2015/src/bin/05.rs
@@ -1,5 +1,28 @@
+use ::y_2015::read_file;
+use regex::Regex;
 
+// const VOWELS: &[char; 5] = &['a', 'e', 'i', 'o', 'u'];
+// const RESTRICTED: &[&str; 4] = &["ab", "cd", "pq", "xy"];
 
 fn main() -> Result<(), std::io::Error> {
+    let data = read_file(5).unwrap();
+    let mut sol = (0_u16, 0);
+
+    let regex_string = r"*(aeiou){3}|^(ab|cd|pq|xy)*";
+    let re = Regex::new(regex_string).unwrap();
+
+    sol.0 = data
+        .lines()
+        .take_while(|line| re.is_match(line))
+        .count() as u16;
+
+    let regex_string = r"";
+    let re = Regex::new(regex_string).unwrap();
+
+    sol.1 = data
+        .lines()
+        .take_while(|line| re.is_match(line))
+        .count() as u16;
+
     Ok(())
 }
